3261493c809b1a0b60fb9a46cbf547735545176b
Tanvir <zishantanvir05@gmail.com>
2024-11-17 15:28:58 +0600

    setup routes

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Welcome to Email password Home </h2>
+        </div>
+    );
+};
+
+export default Home;
